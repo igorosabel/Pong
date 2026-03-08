0c6d9e3e8ec8756ebabfca22e65e9fb22e7746c5
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2026-03-08 18:58:42 +0100

    Actualizo Angular CLI (21.2.0 -> 21.2.1), Angular (21.2.0 -> 21.2.1) y Angular ESLint (21.2.0 -> 21.3.0)

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-// @ts-check
 const eslint = require('@eslint/js');
+const { defineConfig, globalIgnores } = require('eslint/config');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
 
-module.exports = tseslint.config(
+module.exports = defineConfig(
+  globalIgnores(['dist/**', '.angular/**', 'coverage/**']),
   {
     files: ['**/*.ts'],
     extends: [
@@ -36,6 +37,9 @@ module.exports = tseslint.config(
   {
     files: ['**/*.html'],
     extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
-    rules: {},
-  }
+    rules: {
+      '@angular-eslint/template/click-events-have-key-events': 'off',
+      '@angular-eslint/template/interactive-supports-focus': 'off',
+    },
+  },
 );
